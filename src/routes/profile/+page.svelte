e42c894706fbf98f61507a1bc1912b2440ee2ff3
MichaelETPHP <michaeltaye200@gmail.com>
2025-04-01 23:05:21 +0300

    Navigation Update

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,23 +1,4 @@
-<script>
-  import Profile from '../../components/Profile.svelte'; // Corrected path to the Profile component
 
-  // Sample profile data (you can replace this with dynamic data fetching later)
-  const profileData = {
-    name: 'Duran Clayton',
-    title: 'UI/UX Designer',
-    location: 'London, England',
-    avatarUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg',
-    totalRevenue: '$72,573',
-    orders: '3,257',
-    products: '74',
-    coverImageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg',
-  };
-</script>
-
-<div class="profile-page">
-  <h1>Profile</h1>
-  <Profile />
-</div>
 
 <style>
   .profile-page {
